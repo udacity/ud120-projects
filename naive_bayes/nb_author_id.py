80c0ae955022833b3c07e9bcabd296f65ead0b9f
Sebastian Ibarguen <s@cognits.co>
2015-01-31 17:38:30 -0600

    Finished first and second mini project.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 
-""" 
-    this is the code to accompany the Lesson 1 (Naive Bayes) mini-project 
+"""
+    this is the code to accompany the Lesson 1 (Naive Bayes) mini-project
 
     use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -28,7 +28,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time: ", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(labels_test, pred)
+
+print accuracy
 
 #########################################################
-
-
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
+"""
     this is the code to accompany the Lesson 2 (SVM) mini-project
 
     use an SVM to identify emails from the Enron corpus by their authors
-    
+
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -26,7 +26,29 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+linear_kernel_svm = svm.SVC(kernel='rbf', C=10000.)
+
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+linear_kernel_svm.fit(features_train, labels_train)
+print "training time with SVM's linear kernel", time() - t0
+
+t1 = time()
+pred = linear_kernel_svm.predict(features_test)
+print "prediction time with SVM's linear kernel", time() - t1
+
+acc = accuracy_score(labels_test, pred)
+print acc
 
 #########################################################
 
-
+def time_with_power(power, people,times):
+    results = nd.random.power(power, people)
+    for i in range(times):
+            results += nd.random.power(power, 1000)
+    return results
