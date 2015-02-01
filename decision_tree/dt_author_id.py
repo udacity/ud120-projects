34ccead44263f4992a7870caefe038e66a2cfea5
Sebastian Ibarguen <s@cognits.co>
2015-02-01 10:42:06 -0600

    Finished third mini project for decision trees.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
+"""
     this is the code to accompany the Lesson 3 (decision tree) mini-project
 
     use an DT to identify emails from the Enron corpus by their authors
-    
+
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -21,13 +21,20 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+print "Size of features matrix: ", features_train.shape
 
 
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+acc = accuracy_score(labels_test, pred)
+print "Accuracy: ", acc
 #########################################################
-
-
