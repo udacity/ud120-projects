4c0e06b1e0d4928aaaef915c65643b5a8ececc9e
Sebastian Ibarguen <s@cognits.co>
2015-02-03 18:37:45 -0600

    finished 6 mini project

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-""" 
-    starter code for exploring the Enron dataset (emails + finances) 
+"""
+    starter code for exploring the Enron dataset (emails + finances)
     loads up the dataset (pickled dict of dicts)
 
     the dataset has the form
@@ -12,7 +12,7 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
@@ -20,3 +20,13 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 
+print enron_data['KENETH LAY L']
+
+counts_poi=[0,0]
+for key, d in enron_data.iteritems():
+     if d['total_payments'] == 'NaN' and d['poi'] == True:
+             counts_poi[0] += 1
+     elif d['poi'] == True:
+             counts_poi[1] += 1
+
+print counts_poi
--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -2,7 +2,7 @@
 
 """
     starter code for the regression mini-project
-    
+
     loads up/formats a modified version of the dataset
     (why modified?  we've removed some trouble points
     that you'll find yourself in the outliers mini-project)
@@ -11,7 +11,7 @@
 
     you fill in the regression code where indicated
 
-"""    
+"""
 
 
 import sys
@@ -20,9 +20,9 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
-### list the features you want to look at--first item in the 
+### list the features you want to look at--first item in the
 ### list will be the "target" feature
-features_list = ["bonus", "salary"]
+features_list = ["bonus",  "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)#, "long_term_incentive"], remove_any_zeroes=True )
 target, features = targetFeatureSplit( data )
 
@@ -35,22 +35,28 @@ test_color = "b"
 
 
 ### your regression goes here!
-### please name it reg, so that the plotting code below picks it up and 
+### please name it reg, so that the plotting code below picks it up and
 ### plots it correctly
 
+from sklearn.linear_model import LinearRegression
 
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print "Coeff: ", reg.coef_
+print "Intercept: ", reg.intercept_
+print "Score: ", reg.score(feature_train, target_train)
+print "Score: ", reg.score(feature_test, target_test)
 
-
-
+pred = reg.predict(feature_test)
 
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
 for feature, target in zip(feature_test, target_test):
-    plt.scatter( feature, target, color=test_color ) 
+    plt.scatter( feature, target, color=test_color )
 for feature, target in zip(feature_train, target_train):
-    plt.scatter( feature, target, color=train_color ) 
+    plt.scatter( feature, target, color=train_color )
 
 ### labels for the legend
 plt.scatter(feature_test[0], target_test[0], color=test_color, label="test")
@@ -64,6 +70,16 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+# added
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="r")
+print "*********** Switch **********"
+print "Coeff: ", reg.coef_
+print "Intercept: ", reg.intercept_
+print "Score: ", reg.score(feature_train, target_train)
+print "Score: ", reg.score(feature_test, target_test)
+#
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
