5ded9d5c2e406f8b823495b5c1ac8b65b0cbb378
sym44 <raymond.seki@gmail.com>
2015-02-05 17:57:31 +1100

    #Project 3
    update new codes including solution to mini project 3: decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -21,12 +21,21 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
-
-
+print(len(features_train[0]))
 #########################################################
 ### your code goes here ###
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+acc = accuracy_score(labels_test, pred)
+
+print(round(acc, 3))
 
 #########################################################
 
