7291cb7f698c675481cd029bf6b423f3df81487a
Sebastian Ibarguen <s@cognits.co>
2015-02-06 15:34:27 -0600

    Finished 8 mini project

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
-""" 
+"""
     skeleton code for k-means clustering mini-project
 
 """
@@ -41,25 +41,33 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-### there's an outlier--remove it! 
+### there's an outlier--remove it!
 data_dict.pop("TOTAL", 0)
 
 
-### the input features we want to use 
-### can be any key in the person-level dictionary (salary, director_fees, etc.) 
+stocks = []
+for key, value in data_dict.iteritems():
+    if value['salary'] != 'NaN':
+        stocks.append(value['salary'])
+
+print min(stocks), max(stocks)
+
+### the input features we want to use
+### can be any key in the person-level dictionary (salary, director_fees, etc.)
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "bonus"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
 
 ### in the "clustering with 3 features" part of the mini-project,
-### you'll want to change this line to 
+### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
@@ -77,12 +85,9 @@ Draw(pred, finance_features, poi, name="clusters_before_scaling.pdf", f1_name=fe
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+
+
 try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
-
-
-
-
-
