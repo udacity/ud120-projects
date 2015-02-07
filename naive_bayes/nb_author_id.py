abcb9cbe6125dd1100834615e6d6d7a6688d3a3a
Leon van Bokhorst <leon@remondo.net>
2015-02-07 15:59:21 +0100

    First commit

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -10,16 +10,17 @@
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -8,8 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 
 
-
-def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/email_authors.pkl"):
+def preprocess(words_file="../tools/word_data.pkl", authors_file="../tools/email_authors.pkl"):
     """ 
         this function takes a pre-made list of email texts (by default word_data.pkl)
         and the corresponding authors (by default email_authors.pkl) and performs
@@ -28,11 +27,13 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### the words (features) and authors (labels), already largely preprocessed
     ### this preprocessing will be repeated in the text learning mini-project
-    word_data = pickle.load( open(words_file, "r"))
-    authors = pickle.load( open(authors_file, "r") )
+    word_data = pickle.load(open(words_file))
+    authors = pickle.load(open(authors_file))
 
     ### test_size is the percentage of events assigned to the test set (remainder go into training)
-    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors,
+                                                                                                 test_size=0.1,
+                                                                                                 random_state=42)
 
 
 
@@ -40,7 +41,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                                  stop_words='english')
     features_train_transformed = vectorizer.fit_transform(features_train)
-    features_test_transformed  = vectorizer.transform(features_test)
+    features_test_transformed = vectorizer.transform(features_test)
 
 
 
@@ -49,11 +50,10 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     selector = SelectPercentile(f_classif, percentile=10)
     selector.fit(features_train_transformed, labels_train)
     features_train_transformed = selector.transform(features_train_transformed).toarray()
-    features_test_transformed  = selector.transform(features_test_transformed).toarray()
+    features_test_transformed = selector.transform(features_test_transformed).toarray()
 
     ### info on the data
-    print "no. of Chris training emails:", sum(labels_train)
-    print "no. of Sara training emails:", len(labels_train)-sum(labels_train)
-
+    print("no. of Chris training emails:", sum(labels_train))
+    print("no. of Sara training emails:", len(labels_train) - sum(labels_train))
 
     return features_train_transformed, features_test_transformed, labels_train, labels_test
