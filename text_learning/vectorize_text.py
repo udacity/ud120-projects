66fa3f8d53c4c40d2ecf71272b55b241290c44df
Sebastian Ibarguen <s@cognits.co>
2015-02-08 01:22:20 -0600

    Finished10 mini project

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -35,28 +35,39 @@ word_data = []
 ### temp_counter helps you only look at the first 200 emails in the list
 temp_counter = 0
 
-
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
-            path = os.path.join('..', 'tools', path[:-1])
+            path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            content = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            stop_words = ["sara", "shackleton", "chris", "germani"]
+
+            for w in stop_words:
+                content = content.replace(w, "")
 
             ### append the text to word_data
+            word_data.append( content )
+
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
 
 
             email.close()
+
 
 print "emails processed"
 from_sara.close()
@@ -67,8 +78,11 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 
-
-
 ### in Part 4, do TfIdf vectorization here
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english")
 
+vectorizer.fit_transform(word_data)
+print "#feature names: ", len(vectorizer.get_feature_names())
+print vectorizer.get_feature_names()[34590:34600]
