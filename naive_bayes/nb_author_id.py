dd800f4a4c6dfb7cd6028b17c0f9564a9d3a8fae
Leon van Bokhorst <leon@remondo.net>
2015-02-13 18:22:23 +0100

    1. Naive Bayes Classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -23,19 +23,23 @@ from email_preprocess import preprocess
 # features_train and features_test are the features for the training
 # and testing datasets, respectively
 # labels_train and labels_test are the corresponding item labels
+t2 = time()
 features_train, features_test, labels_train, labels_test = preprocess()
-
+print("preprocess time:", round(time()-t2, 3), "s")
 
 # Train a Gaussian Naive Bayes Classifier
 
 clf = GaussianNB()
 t0 = time()
 clf.fit(features_train, labels_train)
-print "training time:", round(time()-t0, 3), "s"
+print("training time:", round(time()-t0, 3), "s")
 
 t1 = time()
 accuracy = clf.score(features_test, labels_test)
-print "prediction time:", round(time()-t1, 3), "s"
+print("prediction time:", round(time()-t1, 3), "s")
 print('Accuracy:', accuracy)
 
 
+# Pre process  : 19.666 secs
+# Training time:  0.713, 0.731, 0.730
+# Predict time :  0.190, 0.192, 0.189
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -16,17 +16,32 @@ sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
+
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 
-
-#########################################################
-### your code goes here ###
-
-#########################################################
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
+for c in [10000.]:
+    clf = SVC(kernel="rbf", C=c)
+    clf.fit(features_train, labels_train)
+
+    pred = clf.predict(features_test)
+    acc = accuracy_score(pred, labels_test)
+    print(c, acc)
+
+    count = 0
+
+    for p in pred:
+        if p == 1:
+            count += 1
+
+    print(count)
