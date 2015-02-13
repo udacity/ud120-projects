ec710aa90d80f13ce83eba96cb87418b7486db37
Sebastian Ibarguen <s@cognits.co>
2015-02-13 17:05:15 -0600

    Finished 12 mini project.

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -70,7 +70,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random
 ###############################################################################
 # Compute a PCA (eigenfaces) on the face dataset (treated as unlabeled
 # dataset): unsupervised feature extraction / dimensionality reduction
-n_components = 150
+n_components = 250
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
@@ -84,6 +84,8 @@ t0 = time()
 X_train_pca = pca.transform(X_train)
 X_test_pca = pca.transform(X_test)
 print "done in %0.3fs" % (time() - t0)
+print "Explained Variance Ration:"
+print pca.explained_variance_ratio_
 
 
 ###############################################################################
@@ -100,6 +102,7 @@ clf = clf.fit(X_train_pca, y_train)
 print "done in %0.3fs" % (time() - t0)
 print "Best estimator found by grid search:"
 print clf.best_estimator_
+
 
 
 ###############################################################################
