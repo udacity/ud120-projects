c6fb1d041228ce2d24787f3b0615941b0fe0a7fd
Leon van Bokhorst <leon@remondo.net>
2015-02-22 18:26:18 +0100

    1. Naive Bayes Classifier

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -14,15 +14,22 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
 
+accuracy = accuracy_score(pred, labels_test)
+print(accuracy)
+print(len(features_train[0]))
 
 #########################################################
 ### your code goes here ###
