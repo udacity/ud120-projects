45584e4f4a61365822c67a29784cad8fff1c79b0
Jayant Sahewal <jayant.sahewal@gmail.com>
2015-08-05 16:20:38 -0400

    Lesson 7 changes completed

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,11 +9,28 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+
+data_dict.pop('TOTAL',0)
+
+#print data_dict.keys()
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
-
+pos = 0
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    #print pos, salary, bonus, bonus/salary
+    matplotlib.pyplot.scatter( salary, bonus )
+
+keys = data_dict.keys()
+
+for key in keys:
+    print key
+    print data_dict[key]['salary']
+    print data_dict[key]['bonus']
+
 
 
 
