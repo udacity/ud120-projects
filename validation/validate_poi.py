471981ad1a9b5e1a7021e0e2b4dd66b31b8f90ac
Lino Coria <lcoria@bbtv.com>
2015-12-30 14:54:30 -0800

    Cross validation
    Added code on cross validation.

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -26,7 +26,17 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+### it's all yours from here forward!
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+        features, labels, test_size=0.30, random_state=42)
 
-### it's all yours from here forward!  
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+print 'Accuracy score = ', accuracy_score(labels_test, pred)
 
