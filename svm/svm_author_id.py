c3f4beffe6b2ac2b10f3155bc1fc0e9899af0845
zero0nee <kristian.ronn.89@gmail.com>
2016-01-31 18:26:47 +0100

    Adding SVM mini-project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,9 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
 
 
 ### features_train and features_test are the features for the training
@@ -24,6 +27,33 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf = svm.SVC(kernel='rbf', C=10000.0)
+
+#test how much faster the SVM is with less training data
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+y_pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+print "Accuracy on testing set:"
+print(accuracy_score(labels_test,y_pred))
+
+##Specific predictions
+prediction = clf.predict(features_test)
+sum = 0
+for i in prediction:
+	if i == 1:
+		sum = sum + 1
+
+print("Number of Chris emails:")
+print(sum)
+
 
 #########################################################
 
