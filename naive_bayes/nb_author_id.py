2aeca7cc408b61469b727b70e279f7272abaeb66
zero0nee <kristian.ronn.89@gmail.com>
2016-01-31 17:37:33 +0100

    Completed NB mini-project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,6 +14,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
 
 
 ### features_train and features_test are the features for the training
@@ -23,11 +25,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
 #########################################################
 ### your code goes here ###
 
+clf = MultinomialNB()
 
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+y_pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+print "Accuracy on testing set:"
+print(accuracy_score(labels_test,y_pred))
 #########################################################
 
 
