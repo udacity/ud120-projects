9faaf6ab9e286bbe3ae8390e12634c842341cfbf
Clement Habinshuti <hello@habbes.xyz>
2016-05-30 22:17:34 +0000

    Fix typo

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-from sklearn.naive_bayes im(port GaussianNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score as accuracy
 
 clf = GaussianNB()
