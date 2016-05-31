f82aec78afcebaf331f8bc1e171c6dfeef5e7ec5
Habbes <haby_habbes@outlook.com>
2016-05-31 21:09:11 +0300

    Reduce training dataset size

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,6 +27,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score as accuracy
 clf = SVC(kernel='linear')
+
+# reduce size of dataset
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
 t0 = time()
 clf.fit(features_train, labels_train)
 print "Training time: ", round(time() - t0, 3)
