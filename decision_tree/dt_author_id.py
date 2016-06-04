1867700668c67608a240baf574724cde7a76fdd1
Habbes <haby_habbes@outlook.com>
2016-06-04 19:45:36 +0300

    Decision tree accuracy

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,6 +24,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier as DTC
+from sklearn.metrics import accuracy_score as accuracy
+clf = DTC(min_samples_split=40)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0,3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time() - t0,3), "s"
+acc = accuracy(labels_test, pred)
+print "accuracy:",acc
 
 
 #########################################################
