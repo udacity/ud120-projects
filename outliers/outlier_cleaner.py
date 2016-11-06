6331fc5ea04e1b462ef1a047b4e9f098b4df855f
sourabh sriom <sourabh.iitg@gmail.com>
2016-11-06 13:03:01 -0800

    outlier cleaner

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import pandas as pd
+import math
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -10,8 +12,23 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
+   # errors = zip(predictions,net_worths)
     
-    cleaned_data = []
+    result = zip(predictions,ages,net_worths)
+    
+    result_df = pd.DataFrame(result, columns = ['predictions', 'ages', 'net_worths'])
+    
+    result_df['errors'] = math.fabs(result_df['net_worths'] - result_df['predictions'])
+    
+    result_sorted = result_df.sort_values(by = 'errors', ascending = True)
+    
+    result_sorted.head(80)
+    
+    print "what is going on?"
+    
+    cleaned_data = zip(result_sorted['ages'], result_sorted['net_worths'], result_sorted['errors'])
+    
+       
 
     ### your code goes here
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -4,6 +4,7 @@ import random
 import numpy
 import matplotlib.pyplot as plt
 import pickle
+import pandas as pd
 
 from outlier_cleaner import outlierCleaner
 
@@ -26,14 +27,16 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn import linear_model
+
+reg = linear_model.LinearRegression()
+
+reg.fit(ages_train,net_worths_train)
 
 
+#print "slope of this regression is %f" %reg.coef_
 
-
-
-
-
-
+#print "score of this regression model is %f" %reg.score(ages_test,net_worths_test)
 
 
 try:
@@ -48,15 +51,13 @@ plt.show()
 cleaned_data = []
 try:
     predictions = reg.predict(ages_train)
+    print pd.DataFrame(predictions,ages_train, net_worths_train)
     cleaned_data = outlierCleaner( predictions, ages_train, net_worths_train )
+    
+    #cleaned_data = [1,2,2,4]
 except NameError:
     print "your regression object doesn't exist, or isn't name reg"
     print "can't make predictions to use in identifying outliers"
-
-
-
-
-
 
 
 ### only run this code if cleaned_data is returning data
@@ -81,4 +82,7 @@ if len(cleaned_data) > 0:
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
+
+
+
 
