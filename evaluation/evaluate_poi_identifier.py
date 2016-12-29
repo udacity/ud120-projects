0c12a6975b00506f5a8e486bd6a5dd8660c613a1
VaibhavSaini1996 <saini.vaibhav1996@gmail.com>
2016-12-29 19:18:04 +0530

    Finished 14th mini project.

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -26,6 +26,46 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### your code goes here 
+### your code goes here
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.tree import DecisionTreeClassifier
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.30, random_state=42)
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+acc = accuracy_score(labels_test, pred)
+
+print "Accuracy: ", acc
+print "No. of POI ", sum(pred)
+print "People in test set", len(pred)
+print "Accuracy of all zeroes", accuracy_score([0]*29, labels_test)
+print "Precision:", precision_score(labels_test, pred)
+print "Recall: ", recall_score(labels_test, pred)
+
+###confusion matrix
+
+from collections import Counter
+confusion_matrix = Counter()
+
+#truth = labels_test
+prediction = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+truth = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+positives = [1]
+
+binary_truth = [x in positives for x in truth]
+binary_prediction = [x in positives for x in prediction]
+for t, p in zip(binary_truth, binary_prediction):
+    confusion_matrix[t,p] += 1
+
+print confusion_matrix
+print "precsion: ", precision_score(truth, prediction)
+print "recall: ", recall_score(truth, prediction)
+
 
 
