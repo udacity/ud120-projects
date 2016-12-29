c76c3ac5687ec616488c6e347b615f069df0ed3e
VaibhavSaini1996 <saini.vaibhav1996@gmail.com>
2016-12-29 18:36:10 +0530

    Finished 1st and 2nd mini project on Naive Bayes.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,6 +26,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "\ntraining time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+accuracy = accuracy_score(pred, labels_test)
+
+print '\naccuracy = {0}' .format(accuracy)
 
 
 #########################################################
