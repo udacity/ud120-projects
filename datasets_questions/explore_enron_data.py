687faf636aec345f70596c58e0255f7e6642b3de
VaibhavSaini1996 <saini.vaibhav1996@gmail.com>
2016-12-29 18:34:14 +0530

    Finished 6th mini project.

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -20,3 +20,32 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 
+#data = enron_data['PRENTICE JAMES']
+#data = enron_data['COLWELL WESLEY'] enron_data['LAY KENNETH L']
+#data = enron_data['FASTOW ANDREW S'] enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+
+##cnt = 0
+##
+##for v in enron_data:
+##    if enron_data[v]['poi'] == True:
+##        cnt+=1
+##
+##print cnt
+
+##count_salary = 0
+##count_email = 0
+##for key in enron_data.keys():
+##    if enron_data[key]['salary'] != 'NaN':
+##        count_salary+=1
+##    if enron_data[key]['email_address'] != 'NaN':
+##        count_email+=1
+##print count_salary
+##print count_email
+
+count_NaN_tp = 0
+for key in enron_data.keys():
+    if enron_data[key]['total_payments'] == 'NaN'and enron_data[key]['poi'] == True:
+        count_NaN_tp+=1
+print count_NaN_tp
+print float(count_NaN_tp)/len(enron_data.keys())
