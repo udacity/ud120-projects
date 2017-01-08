bafa029524ae1878255dc29d6d9452122b29c73a
ZhengshiZhao <zhao.zhengshi@gmail.com>
2017-01-08 18:20:22 -0500

    add Naive Bayes Classification & calculate prediction accuracy

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -30,4 +30,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+def authour_Accuracy(features_train,labels_train,features_test,labels_test):
+	from sklearn.naive_bayes import GaussianNB
+	from sklearn.metrics import accuracy_score
+	clf = GaussianNB()
+	
+	t0 = time()
+	clf.fit(features_train,labels_train)
+	t_fit=round(time()-t0,3)
+	
+	t1 = time()
+	lables_predict = clf.predict(features_test)
+	t_predict = round(time()-t1,3)
+	#accuracy = clf.score(features_test,labels_test)
+	accuracy = round(accuracy_score(labels_test,lables_predict),5)
+	return accuracy,t_fit,t_predict 
 
+print authour_Accuracy(features_train,labels_train,features_test,labels_test)
