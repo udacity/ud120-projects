f8ecff57af3ec5db82cc44193bed2aaa294c77c1
anuj1994 <anujdhoundiyal@gmail.com>
2017-01-19 16:51:56 +0530

    Tutorial task

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -18,7 +18,14 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+from sklearn.svm import SVC
+clf=SVC(kernel="linear")
+clf.fit(features_train, features_test)
+pred=clf.predict(labels_train)
 
+from sklearn.metrics import accuracy_score
+
+accuracy_score(pred,labels_test)
 
 
 
