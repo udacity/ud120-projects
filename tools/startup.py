ca23f31f175525c3d7f220768d579589fb186787
Bernhard Rode <mail@bernhardrode.de>
2017-01-27 13:39:44 +0100

    usage of urllib has changed
    This version uses urllib correctly in python2 and python3.

--- tools/startup.py
+++ tools/startup.py
@@ -31,9 +31,19 @@ print("to check on progress, you can cd up one level, then execute <ls -lthr>")
 print("Enron dataset should be last item on the list, along with its current size")
 print("download will complete at about 423 MB")
 
-import urllib
+
+import sys
+
+if sys.version_info[0] >= 3:
+    from urllib.request import urlretrieve
+else:
+    # Not Python 3 - today, it is most likely to be Python 2
+    # But note that this might need an update when Python 4
+    # might be around one day
+    from urllib import urlretrieve
+
 url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tgz"
-urllib.urlretrieve(url, filename="../enron_mail_20150507.tgz")
+urlretrieve(url, filename="../enron_mail_20150507.tgz")
 print("download complete!")
 
 
