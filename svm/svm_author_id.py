8244ad5b554201434fad1ab82453b7e4b3fa6ce9
RiccardoGrin <grinovericcardo@gmail.com>
2017-05-10 15:03:10 +1000

    Timing prints added, and shortened the training data set
    With regular data set, it tool almost 5min to train

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -23,9 +23,16 @@ from sklearn import svm
     
 clf = svm.SVC(kernel='linear')
 
-clf.fit(features_train, labels_train)
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
 pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
 
 accuracy = clf.score( features_test, labels_test)
 
