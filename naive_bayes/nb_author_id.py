0146aee3e27dac302df360e945c16b7d64ebdcad
zhangge208 <zhangge208@gmail.com>
2017-05-22 17:35:35 +0800

    feat<native bayes>: implement nb_author_id

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,12 +14,27 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+def get_nb_accuracy():
+    features_train, features_test, labels_train, labels_test = preprocess()
+    clf = GaussianNB()
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print "training time:", round(time() - t0, 3), "s"
+
+    t1 = time()
+    clf.predict(features_test)
+    print "predicting time:", round(time() - t1, 3), "s"
+    accuracy = clf.score(features_test, labels_test)
+    return accuracy
+
+
+print get_nb_accuracy()
+
 
 
 
