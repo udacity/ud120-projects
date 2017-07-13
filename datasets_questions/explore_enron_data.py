a790d5268b05a3a6a55600aad69132b2d396655b
Oscar Salvador Morillo Victoria <oscar.morillo@erento.com>
2017-07-13 09:53:20 +0200

    wip

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,27 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+# load dataset into dataframe
+import pandas as pd
+
+df = pd.DataFrame(enron_data)
+print(df.shape)
+
+d = df.transpose()
+print(d.head())
+#df['poi']
+
+#poi_count =
+#print(df.loc['poi'] == True)
+#print("%d people are persons of interest" % poi_count)
+
+with open('../final_project/poi_names.txt', 'r') as f:
+    count = 0
+    for line in f.readlines():
+        if "(y)" in line or "(n)" in line:
+            count += 1
+    print("there were in total %d persons of interest" % count)
+
+
+
 
