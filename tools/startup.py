d25844238c4c063c235865c0eeb53da69d7b2362
KaltovichArtyom <KaltovichArtyom@gmail.com>
2017-08-15 22:08:26 +0300

    Add progress bar while downloading enron dataset

--- tools/startup.py
+++ tools/startup.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
+import time
+import sys
+
+if sys.version_info[0] >= 3:
+    from urllib.request import urlretrieve
+else:
+    # Not Python 3 - today, it is most likely to be Python 2
+    # But note that this might need an update when Python 4
+    # might be around one day
+    from urllib import urlretrieve
 
 print()
 print("checking for nltk")
@@ -32,18 +42,28 @@ print("Enron dataset should be last item on the list, along with its current siz
 print("download will complete at about 423 MB")
 
 
-import sys
-
-if sys.version_info[0] >= 3:
-    from urllib.request import urlretrieve
-else:
-    # Not Python 3 - today, it is most likely to be Python 2
-    # But note that this might need an update when Python 4
-    # might be around one day
-    from urllib import urlretrieve
+def reporthook(count, block_size, total_size):
+    """
+    Callback for displaying progress bar while downloading the enron dataset.
+    Thanks to Pushkaraj Shinde https://github.com/udacity/ud120-projects/pull/55
+    For further information about parameters see documentation of urlretrieve,
+    e.g. https://docs.python.org/3.5/library/urllib.request.html#urllib.request.urlretrieve
+    """
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = int(count * block_size)
+    speed = int(progress_size / (1024 * duration))
+    percent = min(int(count * block_size * 100 / total_size), 100)
+    sys.stdout.write("\rEnron dataset: Downloaded %d%%, %d MB, %d KB/s, %d seconds passed" %
+                    (percent, progress_size / (1024 * 1024), speed, duration))
+    sys.stdout.flush()
 
 url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tgz"
-urlretrieve(url, filename="../enron_mail_20150507.tgz")
+
+urlretrieve(url, "../enron_mail_20150507.tgz", reporthook)
 print("download complete!")
 
 
