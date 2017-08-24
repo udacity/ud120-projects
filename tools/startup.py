8f7c1060a106bca522db6ec6a6c7f64c0a835824
Marwane Jaber <marwane.jaber@anaplan.com>
2017-08-24 12:44:30 +0200

    Download Link is deprecated in startup.py

--- tools/startup.py
+++ tools/startup.py
@@ -31,8 +31,8 @@ print "to check on progress, you can cd up one level, then execute <ls -lthr>"
 print "Enron dataset should be last item on the list, along with its current size"
 print "download will complete at about 423 MB"
 import urllib
-url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tgz"
-urllib.urlretrieve(url, filename="../enron_mail_20150507.tgz") 
+url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
+urllib.urlretrieve(url, filename="../enron_mail_20150507.tar.gz") 
 print "download complete!"
 
 
@@ -41,7 +41,7 @@ print "unzipping Enron dataset (this may take a while)"
 import tarfile
 import os
 os.chdir("..")
-tfile = tarfile.open("enron_mail_20150507.tgz", "r:gz")
+tfile = tarfile.open("enron_mail_20150507.tar.gz", "r:gz")
 tfile.extractall(".")
 
 print "you're ready to go!"
