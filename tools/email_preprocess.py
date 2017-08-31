0a064664461a16891c1015bcb3db03f3804ef244
KaltovichArtyom <KaltovichArtyom@gmail.com>
2017-08-31 21:25:35 +0300

    update email_preprocess to sklearn 0.19

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -4,10 +4,9 @@ import pickle
 import cPickle
 import numpy
 
-from sklearn import cross_validation
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
-
+from sklearn.model_selection import train_test_split
 
 
 def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/email_authors.pkl"):
@@ -39,7 +38,8 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### test_size is the percentage of events assigned to the test set
     ### (remainder go into training)
-    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test =\
+        train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 
 
@@ -59,7 +59,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     features_test_transformed  = selector.transform(features_test_transformed).toarray()
 
     ### info on the data
-    print "no. of Chris training emails:", sum(labels_train)
-    print "no. of Sara training emails:", len(labels_train)-sum(labels_train)
+    print ("no. of Chris training emails:", sum(labels_train))
+    print ("no. of Sara training emails:", len(labels_train)-sum(labels_train))
     
     return features_train_transformed, features_test_transformed, labels_train, labels_test
