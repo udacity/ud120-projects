81bc1d58348d108515afd06fe1b141eaae6d447c
KaltovichArtyom <KaltovichArtyom@gmail.com>
2017-08-31 21:30:55 +0300

    add python3 support to naive bayes, SVM and your own algorithm

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import pickle
-import cPickle
+#import cPickle
 import numpy
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -28,12 +28,12 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### the words (features) and authors (labels), already largely preprocessed
     ### this preprocessing will be repeated in the text learning mini-project
-    authors_file_handler = open(authors_file, "r")
+    authors_file_handler = open(authors_file, "rb")
     authors = pickle.load(authors_file_handler)
     authors_file_handler.close()
 
-    words_file_handler = open(words_file, "r")
-    word_data = cPickle.load(words_file_handler)
+    words_file_handler = open(words_file, "rb")
+    word_data = pickle.load(words_file_handler)
     words_file_handler.close()
 
     ### test_size is the percentage of events assigned to the test set
