55c7754f9f3e75efa79e3e81f1cadfa0ed56cd9d
KaltovichArtyom <KaltovichArtyom@gmail.com>
2018-02-12 20:40:46 +0300

    lesson 14: valication

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -15,7 +15,7 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
