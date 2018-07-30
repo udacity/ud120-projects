83ee9a5fbd4752ab03622872609896d020372222
anuragnidhi0830 <37982267+anuragnidhi0830@users.noreply.github.com>
2018-07-31 01:39:52 +0530

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,24 @@
     Sara has label 0
     Chris has label 1
 """
-    
+   
 import sys
 from time import time
-sys.path.append("../tools/")
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+ sys.path.append("../tools/")
 from email_preprocess import preprocess
-
-
-### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+clf = GaussianNB()
+clf.fit(features_train,labels_train)
+clf.predict(features_test)
 
 
 
 
-#########################################################
-### your code goes here ###
 
-
-#########################################################
 
 
