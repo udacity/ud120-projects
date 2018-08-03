89a8406bd129d9c8689c0c38a163218462e8eae4
Jean-Francois PERELGRITZ <jean-francois.perelgritz@airbus.com>
2018-08-04 00:59:37 +0200

    Premier commit depuis le PC portable

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,8 +48,9 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,14 +59,16 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+from sklearn.cluster import KMeans
 
+pred = KMeans(n_clusters=2).fit_predict(finance_features)
 
 
 ### rename the "name" parameter when you change the number of features
--- tools/startup.py
+++ tools/startup.py
@@ -30,9 +30,9 @@ print "downloading the Enron dataset (this may take a while)"
 print "to check on progress, you can cd up one level, then execute <ls -lthr>"
 print "Enron dataset should be last item on the list, along with its current size"
 print "download will complete at about 423 MB"
-import urllib
-url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
-urllib.urlretrieve(url, filename="../enron_mail_20150507.tar.gz") 
+# import urllib
+# url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
+# urllib.urlretrieve(url, filename="../enron_mail_20150507.tar.gz") 
 print "download complete!"
 
 
@@ -40,7 +40,8 @@ print
 print "unzipping Enron dataset (this may take a while)"
 import tarfile
 import os
-os.chdir("..")
+os.chdir("C:\Users\PERELGRITZ_J\ud120-projects")
+print os.getcwd()
 tfile = tarfile.open("enron_mail_20150507.tar.gz", "r:gz")
 tfile.extractall(".")
 
