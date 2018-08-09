31d2b2daee9ba90eede2fef1cb32f5f64069832d
Jean-Francois PERELGRITZ <jean-francois.perelgritz@airbus.com>
2018-08-09 16:52:18 +0200

    Feature selection quiz 28

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -39,5 +39,23 @@ labels_train   = labels_train[:150]
 
 ### your code goes here
 
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+
+print clf.score(features_test, labels_test)
+
+for i in range (clf.n_features_):
+    if clf.feature_importances_[i] >= .2:
+        print i, clf.feature_importances_[i], vectorizer.get_feature_names()[i]
+
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# import numpy as np
+
+# tf = TfidfVectorizer(input='content', stop_words = 'english')
+# tfidf_matrix =  tf.fit_transform(word_data)
+
+# print tf.get_feature_names()
 
 
