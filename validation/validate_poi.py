5030e0c543d75f1b25fe79154f3b7451d2b5ab5a
Jean-Francois PERELGRITZ <jean-francois.perelgritz@airbus.com>
2018-08-21 17:52:54 +0200

    Validation quiz 18

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -29,9 +29,13 @@ labels, features = targetFeatureSplit(data)
 
 ### it's all yours from here forward!  
 
+from sklearn.model_selection import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=.3, random_state=42)
+
 from sklearn import tree
 
 clf = tree.DecisionTreeClassifier()
-clf = clf.fit(features, labels)
+clf = clf.fit(features_train, labels_train)
 
-print clf.score (features, labels)
+print clf.score (features_test, labels_test)
