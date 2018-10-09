372b39b00340e47fd60c3639acc6d797d01b9992
saharne <39811483+saharne@users.noreply.github.com>
2018-10-08 21:59:49 -0700

    Update dt_author_id.py

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -22,10 +24,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
+
+
 #########################################################
 ### your code goes here ###
 
+clf = tree.DecisinTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+prediction = clf.predict(features_test)
+acc = accuracy_score(labels_test, prediction)
 
+def SubmitAccuracies():
+    return{"acc": round(acc,3)}
 #########################################################
 
 
