6a91165626cafc8650765b256fbf5886e4232501
Stefano Manzini <stefano.manzini@gmail.com>
2018-11-09 11:58:29 +0100

    Update finance_regression.py
    Fixed an import and function call to reflect changes in sklearn. This is NOT available anymore in current sklearn:
    sklearn.cross_validation.train_test_split()
    This is the current module structure (current: 0.20.0):
    sklearn.model_selection.train_test_split()

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -26,7 +26,7 @@ data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
 test_color = "b"
