c7f9e12333f4c258a36a81e2adf537a12a9d33a8
mskdv <41102907+mskdv@users.noreply.github.com>
2019-01-27 21:38:02 +0530

    Update dt_author_id.py

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,13 +1,3 @@
-#!/usr/bin/python
-
-""" 
-    This is the code to accompany the Lesson 3 (decision tree) mini-project.
-
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
-    Sara has label 0
-    Chris has label 1
-"""
-    
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -23,9 +13,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+from sklearn import tree
+t0 = time()
+clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+clf = clf.fit(features_train, labels_train)
+print "Training time:",round(time()-t0,3),"s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "Testing time:", round(time()-t1,3),"s"
 
 
-#########################################################
-
-
+from sklearn.metrics import accuracy_score
+acc_split_40 = accuracy_score(pred,labels_test)
+print "Accuracy is equal to:", (round(acc_split_40,5)*100), "%"
