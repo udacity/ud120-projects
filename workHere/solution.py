1700d7aa8337f1530e725b5317e6a4d3d6c7052a
Luis Diaz <piponazo@gmail.com>
2019-09-02 13:08:34 +0200

    Add template file in which candidates will need to work

---
+++ workHere/solution.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+"""
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+#########################################################
+### your code goes here ###
+
+
+#########################################################
