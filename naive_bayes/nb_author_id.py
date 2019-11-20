961dd91ea57e4bf91cfc6d2efc7f1772b78852f3
Mohan Tejeswar Reddy Matli <30986555+Mohanre710@users.noreply.github.com>
2019-11-20 05:41:17 +0530

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -21,13 +21,26 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GuassianNB
 
+#creating classifier
+clf = GuassianNB()
+
+#training the data in to the classsifier and also finding how much time it tool to run
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+#predicting and finding how much time it took to run
+t0 = time()
+pred = clf.pred(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+#findind the accuracy using accuracy_score
+from sklearn.metrics import accuracy_score
+acccuracy = accuracy_score(labels_train,pred)
+print ("Accuracy obtained: %0.4F %%" % (accuracy*100))
 
 #########################################################
-
-
