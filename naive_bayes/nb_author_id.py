9bcbea38ee0ed5ea72c06b066d56c783d61a6576
Achleshwar <achleshwarluthra6@gmail.com>
2020-03-17 13:56:22 +0530

    Model Ready!
    Created a classifier and trained it on training dataset.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,10 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+clf.fit(features_train,labels_train)
 
 #########################################################
 
