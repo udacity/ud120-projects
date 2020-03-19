cecbad693ee498336c2b6b5f65a9b63070019ccc
Achleshwar <achleshwarluthra6@gmail.com>
2020-03-19 15:09:13 +0530

    Create trying_unix.py
    I did this to deal with unpickling error

---
+++ tools/trying_unix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+convert dos linefeeds (crlf) to unix (lf)
+usage: dos2unix.py 
+"""
+original = "word_data.pkl"
+destination = "word_data_unix.pkl"
+
+content = ''
+outsize = 0
+with open(original, 'rb') as infile:
+    content = infile.read()
+with open(destination, 'wb') as output:
+    for line in content.splitlines():
+        outsize += len(line) + 1
+        output.write(line + str.encode('\n'))
+
+print("Done. Saved %s bytes." % (len(content)-outsize))
