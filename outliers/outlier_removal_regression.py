7d2a303c4228c18093ea7ef178fd5af151219290
Fan_Li <victorlifan@vip.qq.com>
2020-03-23 20:59:39 -0500

    Update outlier_removal_regression.py

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,7 +25,10 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(ages_train,net_worths_train)
+print("slop is:", reg.coef_[0])
 
 
 
@@ -81,4 +84,3 @@ if len(cleaned_data) > 0:
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
