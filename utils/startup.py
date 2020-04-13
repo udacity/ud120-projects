2533b5d8a6eacdf16e59c9e28b56d1b027ad91f5
Taras Savchyn <trsvchn@gmail.com>
2020-04-13 16:24:18 +0300

    Add new startup script

--- utils/startup.py
+++ utils/startup.py
@@ -1,47 +1,73 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-print
-print "checking for nltk"
-try:
-    import nltk
-except ImportError:
-    print "you should install nltk before continuing"
-
-print "checking for numpy"
-try:
-    import numpy
-except ImportError:
-    print "you should install numpy before continuing"
-
-print "checking for scipy"
-try:
-    import scipy
-except:
-    print "you should install scipy before continuing"
-
-print "checking for sklearn"
-try:
-    import sklearn
-except:
-    print "you should install sklearn before continuing"
-
-print
-print "downloading the Enron dataset (this may take a while)"
-print "to check on progress, you can cd up one level, then execute <ls -lthr>"
-print "Enron dataset should be last item on the list, along with its current size"
-print "download will complete at about 423 MB"
-import urllib
-url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
-urllib.urlretrieve(url, filename="../enron_mail_20150507.tar.gz") 
-print "download complete!"
-
-
-print
-print "unzipping Enron dataset (this may take a while)"
-import tarfile
 import os
-os.chdir("..")
-tfile = tarfile.open("enron_mail_20150507.tar.gz", "r:gz")
-tfile.extractall(".")
+import logging
+from urllib.request import urlopen
+import tarfile
 
-print "you're ready to go!"
+logging.basicConfig(format='%(levelname)s: %(message)s')
+logging.getLogger().setLevel(logging.INFO)
+
+LIBS = ['numpy', 'scipy', 'sklearn', 'nltk']  # Libs required for the course
+URL = 'https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz'  # Download link
+FILE_NAME = URL.rsplit('/', 1)[-1]  # Get file name, default download path is ./FILE_NAME
+EXTRACT_TO = '.'  # Path to untar
+MB = 1024 ** 2
+
+
+def check_imports():
+    logging.info('Checking env...')
+    failed = []
+    for i, l in enumerate(LIBS):
+        i, n = i + 1, len(LIBS)
+        step = f'[{i}/{n}]'
+
+        def check_import():
+            logging.info(f'{step} Checking {l}...')
+            try:
+                __import__(l)
+            except ImportError:
+                logging.warning(f'{step} You should install {l} before continuing!')
+                failed.append(l)
+            else:
+                logging.info(f'{step} {l} is fine!')
+            finally:
+                logging.info(f'{step} Done!')
+
+        check_import()
+    logging.warning(f'You should install: {" ".join(failed)} before continuing!') if failed else None
+
+
+def download():
+    logging.info(f'Downloading the Enron dataset to {FILE_NAME} ...')
+    chunk_size, curr_size = 4 * 1024, 0
+    start = 'y'
+    if os.path.exists(FILE_NAME):
+        start = str(input(f'File {FILE_NAME} already exists! Download anyway? y/[n]: ')) or 'n'
+    if start != 'y':
+        return
+    else:
+        with urlopen(URL) as r, open(FILE_NAME, 'wb') as f:
+            fsize = int(r.info()['Content-Length'])
+            info_line = lambda dsize=0: f'{dsize // MB}MB/{fsize // MB}MB'
+
+            print('Progress:', f'{info_line()}', end='')
+            while True:
+                chunk = r.read(chunk_size)
+                if chunk:
+                    f.write(chunk)
+                    curr_size += chunk_size
+                    if curr_size % MB == 0:
+                        print('\b' * len(info_line(curr_size - MB)), info_line(curr_size), sep='', end='', flush=True)
+                else:
+                    print()
+                    break
+        logging.info('Download complete!')
+
+    logging.info(f'Extracting Enron dataset to {EXTRACT_TO} ...')
+    with tarfile.open(FILE_NAME, 'r:gz') as f: f.extractall(EXTRACT_TO)
+    logging.info('You\'re ready to go!')
+
+
+if __name__ == '__main__':
+    [fn() for fn in [check_imports, download]]
