1f44db4e96b9343c2a310fc9e8c2bba05ea1c643
yickminglee <52941636+yickminglee@users.noreply.github.com>
2020-05-05 21:08:20 +0800

    regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -21,7 +21,9 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
+print dictionary
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +31,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,7 +40,19 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print 'Slope: ', reg.coef_
+print 'Intercept: ', reg.intercept_
+
+# Predict the response for test dataset
+target_test_pred = reg.predict(feature_test)
+
+# Model Accuracy, how often is the classifier correct?
+print 'Score (train): ', reg.score(feature_train, target_train)
+print 'Score (test): ', reg.score(feature_test, target_test)
 
 
 
@@ -64,6 +78,10 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print 'Slope: ', reg.coef_
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
