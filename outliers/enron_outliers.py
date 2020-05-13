dd64d86f9c60631a3906eb3477675985a0dedb8b
ravi_ks <ksr1122@gmail.com>
2020-05-13 17:54:34 +0200

    feat: check for outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -14,6 +14,17 @@ data = featureFormat(data_dict, features)
 
 
 ### your code below
+THRESHOLD_SALARY = 500000
 
+for key, data in data_dict.items():
+    if not data["salary"] == 'NaN' and int(data["salary"]) > THRESHOLD_SALARY:
+        print(key)
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
