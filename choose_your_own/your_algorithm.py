fee3bfb302b0ce20dbedfd76db0a3000d2e01860
ravi_ks <ksr1122@gmail.com>
2020-05-13 10:56:41 +0200

    feat: k nearest neighbor mini project

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -31,12 +31,14 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.neighbors import KNeighborsClassifier
 
+clf = nbrs = KNeighborsClassifier(n_neighbors=2, algorithm='ball_tree')
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
-
-
-
+from sklearn.metrics import accuracy_score
+print("K Nearest neighbors Accuracy: {:.2f}".format(accuracy_score(pred, labels_test)))
 
 try:
     prettyPicture(clf, features_test, labels_test)
