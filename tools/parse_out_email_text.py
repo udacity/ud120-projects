4e79f002530bc28ed986f384283e52f7252d6eb1
ravi_ks <ksr1122@gmail.com>
2020-05-14 14:49:12 +0200

    feat: implement snowball stemming

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,15 +28,13 @@ def parseOutText(f):
         text_string = content[1].translate(str.maketrans("", "", string.punctuation))
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
-
+        stemmer = SnowballStemmer("english")
+        words = ' '.join([stemmer.stem(txt) for txt in text_string.split()])
 
     return words
 
