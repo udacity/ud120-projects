52da8b095099ac577b4536b2d82338877c84584a
Trent Park <trent.park@bsg.co.za>
2020-06-18 20:32:43 +0200

    Complete decision tree tutorial

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,7 +25,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
+clf = DecisionTreeClassifier(criterion='entropy', min_samples_split=40)
+clf.fit(features_train, labels_train)
+
+labels_pred = clf.predict(features_test)
+
+acc = accuracy_score(labels_test, labels_pred)
+
+# Get the no. features by extracting the no. of columns in the training data
+print "The number of features in the training data is {0}".format(len(features_train[0]))
+
+print "\nThe Decision Tree accuracy is: {0}\n".format(acc)
 
 #########################################################
 
