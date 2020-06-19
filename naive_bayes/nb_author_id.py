ddce57ea74ddf3f6d4ababbb6d5134b0af5f774d
Kuldeep singh <61626717+kuldeepsingh27@users.noreply.github.com>
2020-06-19 23:54:33 +0530

    kuldeepsingh27/ud120-projects
    complete this code for naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+cls= GaussianNB()
+cls.fit(features_train, labels_train)
+pred= cls.predict(features_test)
+accuracy= accuracy_score(pred, labels_test)
 
 #########################################################
 
