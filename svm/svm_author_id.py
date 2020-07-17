5318a80dba8a195d52d5b01e88b75a00105f647e
Ayush Pandey <59275479+Ayush7-BIT@users.noreply.github.com>
2020-07-17 13:54:57 +0530

    Updated svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.svm import SVC
+clf=SVC(kernel="linear")
+clf.fit(features_train,labels_train)
+SVC()
+pred=svc.pred(feautures_test)
 #########################################################
 
 
