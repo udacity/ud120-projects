456a36271161b6c76e07a291676ff9cd12359a17
abhiishekporwal <abhiishekporwal@gmail.com>
2020-07-29 02:51:30 +0530

    corrections

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -9,8 +9,8 @@ from outlier_cleaner import outlierCleaner
 
 
 ### load up some practice data with outliers in it
-ages = pickle.load( open("practice_outliers_ages.pkl", "rb") )
-net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "rb") )
+ages = pickle.load(open("practice_outliers_ages.pkl", "rb"), fix_imports=True)
+net_worths = pickle.load(open("practice_outliers_net_worths.pkl", "rb"), fix_imports=True)
 
 
 
