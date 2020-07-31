1bf6351b4e44d251dca363b94b83689a1e514694
ritumalhotra <ritu_malhotra@hotmail.com>
2020-07-31 21:12:13 +0100

    naive_bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,31 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+# create classifer
+clf = GaussianNB()
 
+# note time
+t0 = time()
+
+# fit the classifier on  training features and labels
+clf.fit(features_train, labels_train)
+print("Training time", time()-t0, "s")
+
+# note time
+t1=time()
+
+# predict labels for the test features
+pred = clf.predict(features_test)
+print("Predicting time", time()-t1, "s")
+
+# calculate accuracy
+accuracy = accuracy_score(pred, labels_test)
+
+# return the accuracy
+# return(accuracy)
+
+print(accuracy)
+# return the accuracy
 
 #########################################################
 
