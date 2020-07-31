020ec308341c192efeddb78dff9a18bdc57818e5
ritumalhotra <ritu_malhotra@hotmail.com>
2020-07-31 21:03:46 +0100

    Svm exercises

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+#clf = SVC(kernel='linear')
+clf = SVC(kernel='rbf', C=10000)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print( "training time:", round(time()-t0, 3), "s")
+
+t1 = time()
+pred = clf.predict(features_test)
+print( "Prediction time:", round(time()-t1, 3), "s")
+
+print( "Accuracy score:", accuracy_score(labels_test, pred))
+
+print( "Predictions for 10:", pred[10], print "Predictions for 26:", pred[26], print "Predictions for 50:", pred[50])
+
+c = Counter(pred)
+print "Number of predictions for Chris(1):", c[1]
 #########################################################
 
 
