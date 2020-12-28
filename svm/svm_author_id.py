864fa3670f420fcb355d1d401a4564f7fa200ceb
Ifeanyi30 <liamsanctus30@gmail.com>
2020-12-28 21:55:23 +0100

    updated the svm code

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+clf = svm.SVM(kernel='linear')
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+accuracy = accuracy_score(labels_test, pred)
 #########################################################
 
 
