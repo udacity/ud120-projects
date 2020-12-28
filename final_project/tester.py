bc2d18e1653e55602b59634a6ad94fb1e86abc4c
Oscar Mauricio Forero Carrillo <oforero@ieee.org>
2020-12-28 11:13:37 +0800

    Open pickle files in binary mode

--- final_project/tester.py
+++ final_project/tester.py
@@ -79,19 +79,19 @@ DATASET_PICKLE_FILENAME = "my_dataset.pkl"
 FEATURE_LIST_FILENAME = "my_feature_list.pkl"
 
 def dump_classifier_and_data(clf, dataset, feature_list):
-    with open(CLF_PICKLE_FILENAME, "w") as clf_outfile:
+    with open(CLF_PICKLE_FILENAME, "wb") as clf_outfile:
         pickle.dump(clf, clf_outfile)
-    with open(DATASET_PICKLE_FILENAME, "w") as dataset_outfile:
+    with open(DATASET_PICKLE_FILENAME, "wb") as dataset_outfile:
         pickle.dump(dataset, dataset_outfile)
-    with open(FEATURE_LIST_FILENAME, "w") as featurelist_outfile:
+    with open(FEATURE_LIST_FILENAME, "wb") as featurelist_outfile:
         pickle.dump(feature_list, featurelist_outfile)
 
 def load_classifier_and_data():
-    with open(CLF_PICKLE_FILENAME, "r") as clf_infile:
+    with open(CLF_PICKLE_FILENAME, "rb") as clf_infile:
         clf = pickle.load(clf_infile)
-    with open(DATASET_PICKLE_FILENAME, "r") as dataset_infile:
+    with open(DATASET_PICKLE_FILENAME, "rb") as dataset_infile:
         dataset = pickle.load(dataset_infile)
-    with open(FEATURE_LIST_FILENAME, "r") as featurelist_infile:
+    with open(FEATURE_LIST_FILENAME, "rb") as featurelist_infile:
         feature_list = pickle.load(featurelist_infile)
     return clf, dataset, feature_list
 
