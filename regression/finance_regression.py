c1081b040523644321694ebaf1eff5655b487605
Ifeanyi30 <liamsanctus30@gmail.com>
2021-03-25 20:18:59 +0100

    encode the newline character to \n in the binary file

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -17,7 +17,7 @@ import sys
 import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "rb") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
