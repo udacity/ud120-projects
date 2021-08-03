8cb1360cba04b795137727bf808cd5923094e83a
Siddharth Kekre <40489787+iSiddharth20@users.noreply.github.com>
2021-08-03 17:32:12 +0530

    Migrated Entire Program to Python-3 with Documentation (#302)
    * Refer ChangeLog: Lecture 01
    
    * Refer ChangeLog: Lecture 02
    
    * Refer ChangeLog: Lecture 03
    
    * Refer ChangeLog: Lecture 06
    
    * Refer ChangeLog: Lecture 07
    
    * Refer ChangeLog: Lecture 08
    
    * Refer ChangeLog: Lecture 09
    
    * Refer ChangeLog: Lecture 11
    
    * Refer ChangeLog: Lecture 12
    
    * Refer ChangeLog: Lecture 13
    
    * Initial Commit: .gitignore
    
    * Initial Commit: ReadMe
    
    * Initial Commit: Requirements.TXT
    
    * Initial Commit: ChangeLog by iSiddharth
    
    Co-authored-by: Siddharth Kekre <isiddharth@iSiddharthMacBook.local>

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ud120-projects
 ==============
 
 Starter project code for students taking Udacity ud120
+
+### Migrated to Python-3 by [Siddharth Kekre](https://github.com/iSiddharth20)
+
+### Please refer to the [Change Log](https://github.com/iSiddharth20/ud120-projects/blob/master/CHANGELOG.md) for Details.
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from nltk.stem.snowball import SnowballStemmer
 import string
@@ -25,16 +25,16 @@ def parseOutText(f):
     words = ""
     if len(content) > 1:
         ### remove punctuation
-        text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
+        text_string = content[1].translate(str.maketrans('','',string.punctuation))
 
         ### project part 2: comment out the line below
         words = text_string
 
+
+
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
 
 
 
@@ -45,7 +45,7 @@ def parseOutText(f):
 def main():
     ff = open("../text_learning/test_email.txt", "r")
     text = parseOutText(ff)
-    print text
+    print(text)
 
 
 
