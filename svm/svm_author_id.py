f490c2dd5a833b218f046934b1357230c12840bb
Augustine <austinnoah92@gmail.com>
2021-08-11 02:15:30 +0100

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,7 +10,7 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("C:/Users/Austin/udacity/ud120-projects/svm")
 from email_preprocess import preprocess
 
 
@@ -19,10 +19,17 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
+
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
 
+clf = SVC(kernel = 'rbf', C = 10000.0)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
 #########################################################
 
@@ -33,7 +40,31 @@ But the Code provided in Quiz has an Indexing issue
 The Code Below solves that issue, So use this one
 '''
 
-# features_train = features_train[:int(len(features_train)/100)]
-# labels_train = labels_train[:int(len(labels_train)/100)]
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+def submitAccuracy():
+    return acc
+##checking for Chris prediction    
+count_ = 0
+for j in pred:
+    if j == 1:
+        count_ += 1
+    else:
+        count_ == 0
+
+print(count_)
+    
+print("The accuracy score is {}".format(submitAccuracy()))
+print(pred[10], pred[26], pred[50])
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+
+t0 = time()
+clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
 
 #########################################################
