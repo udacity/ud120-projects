5fce9cdde2bb4fb1f3e45c95928d16e18a3bedb3
Augustine <austinnoah92@gmail.com>
2021-08-14 19:23:27 +0100

    Update dt_author_id.py

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -10,14 +10,13 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("C:/Users/Austin/udacity/ud120-projects/naive_bayes")
 from email_preprocess import preprocess
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
@@ -25,7 +24,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+features_train, features_test, labels_train, labels_test = preprocess()
+#print(len(features_train[0]))
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf = DecisionTreeClassifier(min_samples_split = 40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+acc = accuracy_score(pred, labels_test)
+
+print('The accuracy score is {}'.format(acc))
 #########################################################
 
 
