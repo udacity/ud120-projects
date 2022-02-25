652eab2e974b638b35b7b3f764e499c65ac7de57
Charith Atapattu <31107032+charithsiu@users.noreply.github.com>
2022-02-25 16:31:39 -0600

    Completed Quiz1

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,6 +14,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -24,8 +26,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 ##############################################################
 # Enter Your Code Here
-
-
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+t0 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
+accuracy = accuracy_score(pred, labels_test)
+print(accuracy)
+print(clf.score(features_test,labels_test))
 
 ##############################################################
 
@@ -43,5 +53,12 @@ The Following Code is Python-3 version of the same code
 # t0 = time()
 # # < your clf.predict() line of code >
 # print("Predicting Time:", round(time()-t0, 3), "s")
+
+#No. of Chris training emails :  7936
+#No. of Sara training emails :  7884
+#Training Time: 1.186 s
+#Predicting Time: 0.209 s
+#0.9732650739476678
+#0.9732650739476678
 
 ##############################################################
