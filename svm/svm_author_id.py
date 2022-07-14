0b776b5aae3889b37b0a043dfba09c19dcde54d3
Darpan-Balar <bs20b013@smail.iitm.ac.in>
2022-07-14 12:58:40 +0530

    SVM mini project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -10,9 +10,10 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("/Users/darpan/Desktop/ud120-projects/tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -22,8 +23,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
 
 
+clf = SVC(kernel = 'rbf', C = 10000)
+t0 = time()
+clf.fit(features_train, labels_train)
+print("fitting time:", round(time()-t0, 3), "s")
+
+t0 = time()
+pred = clf.predict(features_test)
+print("Prediction time:", round(time()-t0, 3), "s")
+
+
+#print(pred[10])
+#print(pred[26])
+#print(pred[50])
+x = len(pred)
+print(sum(clf.predict(features_test)==1)) #number of Chrises in the predicted test set
+accuracy = accuracy_score(pred, labels_test)
+print(accuracy)
 #########################################################
 
 #########################################################
