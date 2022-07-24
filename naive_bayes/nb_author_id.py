b0cb6223a61f93cab7f0565e797181340980f2e8
gabriellefundes <gabriel.lefundes@unesp.br>
2022-07-23 23:39:36 -0300

    My attempt

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -24,7 +24,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 ##############################################################
 # Enter Your Code Here
-
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
 
 ##############################################################
@@ -36,12 +37,13 @@ The Code Given on Udacity Website is in Python-2
 The Following Code is Python-3 version of the same code
 '''
 
-# t0 = time()
-# # < your clf.fit() line of code >
-# print("Training Time:", round(time()-t0, 3), "s")
+t0 = time()
+clf.fit(features_train,labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
 
-# t0 = time()
-# # < your clf.predict() line of code >
-# print("Predicting Time:", round(time()-t0, 3), "s")
+t0 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
 
+print(clf.score(features_test, labels_test))
 ##############################################################
