0cce0e2998793d4a60e76c00af312c976b5ebcfa
Ahmed Sherif <eng_ahmed.sherif@yahoo.com>
2022-08-29 21:05:21 +0200

    1ST project
    Solving Naive_bayes project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -25,6 +25,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ##############################################################
 # Enter Your Code Here
 
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
 
 ##############################################################
@@ -36,12 +38,15 @@ The Code Given on Udacity Website is in Python-2
 The Following Code is Python-3 version of the same code
 '''
 
-# t0 = time()
-# # < your clf.fit() line of code >
-# print("Training Time:", round(time()-t0, 3), "s")
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
 
-# t0 = time()
-# # < your clf.predict() line of code >
-# print("Predicting Time:", round(time()-t0, 3), "s")
+t1 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t1, 3), "s")
 
 ##############################################################
+
+accuracy = clf.score(features_test, labels_test)
+print(accuracy)
--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -36,7 +36,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
 
     ### test_size is the percentage of events assigned to the test set
     ### (remainder go into training)
-    features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.5, random_state=42)
 
 
     ### text vectorization--go from strings to lists of numbers
