d1721874ebc86062e44a381428411c4874282695
Ahmed Sherif <eng_ahmed.sherif@yahoo.com>
2022-09-10 22:13:21 +0200

    Decision_Tree
    Finish Decision Tree Quizes

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -10,22 +10,25 @@
     
 import sys
 from time import time
-sys.path.append("../tools/")
+# sys.path.append("../tools/")
+sys.path.append("..\\tools\\")
 from email_preprocess import preprocess
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
+# features_train, features_test, labels_train, labels_test = preprocess()
+features_train, features_test, labels_train, labels_test = preprocess(words_file = "..\\tools\\word_data_unix.pkl", authors_file="..\\tools\\email_authors.pkl")
 
+print("Number of training features =", len(features_train[0]))
 
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+clf = clf.fit(features_train, labels_train)
 
+acc = clf.score(features_test, labels_test)
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
+print("accuracy with samples = 40 is:", acc)
 
 
--- tools/startup.py
+++ tools/startup.py
@@ -1,45 +1,48 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 
-print("Checking for nltk")
-try:
-    import nltk
-except ImportError:
-    print("You should install nltk before continuing")
+# print("Checking for nltk")
+# try:
+#     import nltk
+# except ImportError:
+#     print("You should install nltk before continuing")
 
-print("Checking for numpy")
-try:
-    import numpy
-except ImportError:
-    print("You should install numpy before continuing")
+# print("Checking for numpy")
+# try:
+#     import numpy
+# except ImportError:
+#     print("You should install numpy before continuing")
 
-print("Checking for scipy")
-try:
-    import scipy
-except:
-    print("You should install scipy before continuing")
+# print("Checking for scipy")
+# try:
+#     import scipy
+# except:
+#     print("You should install scipy before continuing")
 
-print("Checking for sklearn")
-try:
-    import sklearn
-except:
-    print("You should install sklearn before continuing")
+# print("Checking for sklearn")
+# try:
+#     import sklearn
+# except:
+#     print("You should install sklearn before continuing")
 
-print("Downloading the Enron dataset (this may take a while)")
-print("To check on progress, you can cd up one level, then execute <ls -lthr>")
-print("Enron dataset should be last item on the list, along with its current size")
-print("Download will complete at about 1.82 GB")
+# print("Downloading the Enron dataset (this may take a while)")
+# print("To check on progress, you can cd up one level, then execute <ls -lthr>")
+# print("Enron dataset should be last item on the list, along with its current size")
+# print("Download will complete at about 1.82 GB")
 
-import requests
-url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
-filename = "../enron_mail_20150507.tar.gz"
-with open(filename, "wb") as f:
-    r = requests.get(url)
-    f.write(r.content)
-print("Download Complete!")
+# import requests
+# url = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
+# filename = "../enron_mail_20150507.tar.gz"
+# with open(filename, "wb") as f:
+#     r = requests.get(url)
+#     f.write(r.content)
+# print("Download Complete!")
 
-print("Unzipping Enron dataset (This may take a while)")
+# print("Unzipping Enron dataset (This may take a while)")
+print("Openning Enron dataset !")
 import tarfile
-tfile = tarfile.open("../enron_mail_20150507.tar.gz")
+# tfile = tarfile.open("../enron_mail_20150507.tar.gz")
+tfile = tarfile.open("..\enron_mail_20150507.tar.gz")
+print("Unzipping Enron dataset (This may take a while)")
 tfile.extractall(".")
 tfile.close()
 
